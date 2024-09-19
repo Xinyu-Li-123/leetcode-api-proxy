6075b5c2c92b374b3078abc8025af487e056f783
Xinyu Li <eigerli@outlook.com>
2024-09-18 21:26:24 -0400

    create proxy for leetcode api

---
+++ api/leetcode.js
@@ -0,0 +1,34 @@
+// api/leetcode.js
+import fetch from 'node-fetch';
+
+export default async (req, res) => {
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.setHeader('Access-Control-Allow-Methods', 'POST'); // Allow POST
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
+    return res.status(200).end(); // Respond to OPTIONS request with success
+  }
+
+  if (req.method === 'POST') {
+    try {
+      const response = await fetch('https://leetcode.com/graphql/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req.body),
+      });
+
+      const data = await response.json();
+      res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin for actual POST response
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error('Error fetching data from Leetcode:', error.message);
+      return res.status(500).json({ error: `Error fetching data from Leetcode: ${error.message}` });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+};
+
